92a9af8740a207931e00cb4a0584f1ab551e97b9
boiu <416760980@qq.com>
2018-05-03 15:46:09 +0800

    新增Rxjava

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Lib
 MVP模式，底层框架
+#5.3 新增了 RxJava 和 retrofit2 的使用,有简单的使用示例
--- app/src/main/java/com/lj/project/kotlin/KotlinActivity.kt
+++ app/src/main/java/com/lj/project/kotlin/KotlinActivity.kt
@@ -1,25 +1,22 @@
 package com.lj.project.kotlin
 
-import butterknife.BindView
-import butterknife.ButterKnife
-import com.app.base.lib.base.BaseActivity
-import com.lj.project.newljproject.R
 
 /**
+ * 想学习下Kotlin
  * Created by Administrator on 2018/5/3.
  */
-class KotlinActivity : BaseActivity(){
-    @BindView(R.id.txt_msg)
-    val txt_msg = null;
-    override fun setViewAndData() {
-        ButterKnife.bind(this);
-    }
-
-    override fun getLayoutId(): Int {
-        return R.layout.rxjava;
-    }
-
-    override fun initPersenter() {
-    }
-
-}
+//class KotlinActivity : BaseActivity(){
+//    @BindView(R.id.txt_msg)
+//    val txt_msg = null;
+//    override fun setViewAndData() {
+//        ButterKnife.bind(this);
+//    }
+//
+//    override fun getLayoutId(): Int {
+//        return R.layout.rxjava;
+//    }
+//
+//    override fun initPersenter() {
+//    }
+//
+//}
